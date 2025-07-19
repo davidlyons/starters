e44622cdc4c80b85fca8c4c069a866373d765106
David Lyons <david.scott.lyons@gmail.com>
2025-07-18 20:01:06 -0700

    squash

---
+++ readme.md
@@ -0,0 +1,3 @@
+Three.js, React Three Fiber and Tailwind web project starter repos
+
+Minimal, single file for quick prototyping or Vite and TypeScript for more advanced projects.
---
+++ tailwind-react-vite-ts-starter/src/App.tsx
@@ -0,0 +1,17 @@
+import { PiGlobeHemisphereWestLight } from 'react-icons/pi'
+
+function App() {
+  return (
+    <>
+      <div className="py-14">
+        <div className="container">
+          <h1 className="text-4xl">
+            Hello world <PiGlobeHemisphereWestLight />
+          </h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
