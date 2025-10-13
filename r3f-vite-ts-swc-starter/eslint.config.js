2025508a97427d90ff19c94849815fa0f41ef770
David Lyons <david.scott.lyons@gmail.com>
2025-10-13 13:06:25 -0700

    update deps, added format and lint scripts

--- r3f-vite-ts-swc-starter/eslint.config.js
+++ r3f-vite-ts-swc-starter/eslint.config.js
@@ -1,11 +1,12 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import prettier from 'eslint-config-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
@@ -20,4 +21,5 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  prettier,
 ])
