5d256992fae8e0771a383d16ac4abad3b092e378
David Lyons <david.scott.lyons@gmail.com>
2026-06-29 23:50:30 -0700

    update tw starter deps

--- tailwind-react-vite-ts-starter/src/main.tsx
+++ tailwind-react-vite-ts-starter/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import '@fontsource-variable/inter'
+// Supports weights 100-900
+import '@fontsource-variable/inter/wght.css';
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
